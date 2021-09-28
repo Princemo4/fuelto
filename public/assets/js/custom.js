3515e887b45641e949531604e6176f883b21623a
Mohamed Nasr <princemo4@yahoo.com>
2021-09-28 08:34:50 -0400

    Add cover letter text copy to clipboard section

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,110 +1,19 @@
 
-let camera_button = document.querySelector("#start-camera");
-let video = document.querySelector("#recorder-video");
-let start_button = document.querySelector("#start-record");
-let stop_button = document.querySelector("#stop-record");
-let save_video = document.querySelector("#save-video");
+// Show url of video in the employee cover letter body template
+let current_video_url_element = document.querySelector('#current_video_url')
+current_video_url_element.setAttribute('href', window.location.href)
+current_video_url_element.innerText = window.location.href
 
-stop_button.disabled = true;
-start_button.disabled = true;
-save_video.disabled = true;
-
-let camera_stream = null;
-let media_recorder = null;
-let blobs_recorded = [];
-let options = {
-  audio: {
-    sampleRate: 48000,
-    channelCount: 2,
-    volume: 1.0,
-    echoCancellation: true
-  },
-  video: true
+// Copy cover letter body template to clipboard
+let copy_clipboard_btn = document.querySelector('#copy-clipboard-btn')
+let cover_letter_text = document.querySelector('#cover-letter-text')
+copy_clipboard_btn.onclick = () => {
+  let tmp_textarea = document.createElement('textarea');
+  document.querySelector('body').appendChild(tmp_textarea);
+  tmp_textarea.value = cover_letter_text.innerText;
+  tmp_textarea.select();
+  document.execCommand('copy');
+  tmp_textarea.remove();
+  copy_clipboard_btn.innerText = 'COPIED'
+  copy_clipboard_btn.disabled = true;
 }
-let video_local;
-let blob_file;
-// let did_camera_start = () => {
-//   if (!camera_stream) {
-//     alert('Click Start Camera First!')
-//     return false
-//   }
-//   return true
-// };
-
-async function startCamera() {
-  start_button.disabled = false;
-  camera_stream = await navigator.mediaDevices.getUserMedia(options);
-  if (!camera_stream) {
-    alert('Something went wrong with your camera device')
-    throw new Error('Something went wrong with your camera device')
-  }
-  video.srcObject = camera_stream;
-  camera_button.removeEventListener('click', ev1)
-  camera_button.style.display = 'none'
-  video.removeEventListener('click', ev1)
-
-  start_button.addEventListener('click', function() {
-    blobs_recorded = []; //clear previous recording
-    start_button.innerText = 'RECORDING....'
-    start_button.disabled = true
-    stop_button.disabled = false
-    save_video.disabled = true
-    // set MIME type of recording as video/webm
-    media_recorder = new MediaRecorder(camera_stream, { mimeType: 'video/webm;codecs=h264' });
-  
-    // event : new recorded video blob available 
-    media_recorder.addEventListener('dataavailable', function(e) {
-      blobs_recorded.push(e.data);
-    });
-  
-    // start recording with each recorded blob having 1 second video
-    media_recorder.start(1000);
-  });
-  
-  stop_button.addEventListener('click', function() {
-    media_recorder.stop(); 
-    blob_file = new Blob(blobs_recorded, { type: 'video/mp4' })
-    video_local = URL.createObjectURL(blob_file);
-    
-    console.log(video_local)
-    // save_video.href = video_local;
-    start_button.innerText = 'Delete & Record Again'
-    start_button.disabled = false;
-    stop_button.disabled = true;
-    save_video.disabled = false;
-  });
-  
-  save_video.addEventListener('click', function() {
-    console.log('click')
-    let formData = new FormData();
-  
-    formData.append('file', blob_file, 'henry.webm')
-    formData.append('full_name', 'Mohamed')
-    console.log(formData)
-    $.ajax({
-      url: '/upload',
-      method: 'POST',
-      data: formData,
-      contentType: false,
-      processData: false,
-      success: (res) => {
-        response = JSON.parse(JSON.stringify(res))
-        sessionStorage.setItem('fileName', response.fileName)
-        window.location = response.go_to + "?fileName=" + response.fileName
-      }
-    })
-  })
-
-}
-
-function ev1() {
-  // this function was named so it can be referenced in the 
-  // .removeEventListener()
-  startCamera();
-}
-camera_button.addEventListener('click', ev1)
-
-video.addEventListener('click', ev1)
-
-
-
